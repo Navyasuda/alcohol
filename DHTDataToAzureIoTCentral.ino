32f59a500de7069a1fa4cade241a752e8f8dd826
Navyasuda <128464196+Navyasuda@users.noreply.github.com>
2023-05-15 13:37:38 +0530

    Add files via upload

---
+++ DHTDataToAzureIoTCentral.ino
@@ -0,0 +1,103 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full
+// license information.
+
+#include <ESP8266WiFi.h>
+#include "src/iotc/common/string_buffer.h"
+#include "src/iotc/iotc.h"
+//#include "DHT.h"
+
+//#define DHTPIN 2  //D4
+
+//#define DHTTYPE DHT11   // DHT 11
+int alcohol = D1;    
+
+#define WIFI_SSID "Alcohol"
+#define WIFI_PASSWORD "12345678"
+ 
+const char* SCOPE_ID = "One009B4818";
+const char* DEVICE_ID = "14iet93wni8";
+const char* DEVICE_KEY = "sSEM1uRX3qRmmLDw/wVJg7Z1CFDaTD3gT5VVFQj/+V8=";
+
+//DHT dht(DHTPIN, DHTTYPE);
+
+void on_event(IOTContext ctx, IOTCallbackInfo* callbackInfo);
+#include "src/connection.h"
+
+void on_event(IOTContext ctx, IOTCallbackInfo* callbackInfo) {
+  // ConnectionStatus
+  if (strcmp(callbackInfo->eventName, "ConnectionStatus") == 0) {
+    LOG_VERBOSE("Is connected ? %s (%d)",
+                callbackInfo->statusCode == IOTC_CONNECTION_OK ? "YES" : "NO",
+                callbackInfo->statusCode);
+    isConnected = callbackInfo->statusCode == IOTC_CONNECTION_OK;
+    return;
+  }
+
+  // payload buffer doesn't have a null ending.
+  // add null ending in another buffer before print
+  AzureIOT::StringBuffer buffer;
+  if (callbackInfo->payloadLength > 0) {
+    buffer.initialize(callbackInfo->payload, callbackInfo->payloadLength);
+  }
+
+  LOG_VERBOSE("- [%s] event was received. Payload => %s\n",
+              callbackInfo->eventName, buffer.getLength() ? *buffer : "EMPTY");
+
+  if (strcmp(callbackInfo->eventName, "Command") == 0) {
+    LOG_VERBOSE("- Command name was => %s\r\n", callbackInfo->tag);
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(alcohol, INPUT);  
+
+  connect_wifi(WIFI_SSID, WIFI_PASSWORD);
+  connect_client(SCOPE_ID, DEVICE_ID, DEVICE_KEY);
+
+  if (context != NULL) {
+    lastTick = 0;  // set timer in the past to enable first telemetry a.s.a.p
+  }
+  // dht.begin();
+}
+
+void loop() {
+
+//float h = dht.readHumidity();
+//float t = dht.readTemperature();
+int alcohol_data = digitalRead(alcohol);
+
+
+  if (isConnected) {
+
+    unsigned long ms = millis();
+    if (ms - lastTick > 10000) {  // send telemetry every 10 seconds
+      char msg[64] = {0};
+      int pos = 0, errorCode = 0;
+
+      lastTick = ms;
+      if (loopId++ % 2 == 0) {  // send telemetry
+        pos = snprintf(msg, sizeof(msg) - 1, "{\"Alcohol\": %f}",
+                       alcohol_data);
+        errorCode = iotc_send_telemetry(context, msg, pos);
+                
+      } else {  // send property
+        
+      } 
+  
+      msg[pos] = 0;
+
+      if (errorCode != 0) {
+        LOG_ERROR("Sending message has failed with error code %d", errorCode);
+      }
+    }
+
+    iotc_do_work(context);  // do background work for iotc
+  } else {
+    iotc_free_context(context);
+    context = NULL;
+    connect_client(SCOPE_ID, DEVICE_ID, DEVICE_KEY);
+  }
+
+}
